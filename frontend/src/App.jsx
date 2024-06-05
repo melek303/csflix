36521397be928435e9701d6d041eeb78b07dd784
Zouari Wassim <wassim.zouari@student-cs.fr>
2024-06-05 11:17:41 +0200

    test

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,21 +1,12 @@
-import { Route, Routes } from 'react-router-dom';
-import Home from './pages/Home/Home';
-import About from './pages/About/About';
-import Layout from './components/Layout/Layout';
-import Counter from './pages/Counter/Counter';
-import Users from './pages/Users/Users';
+import React from 'react'
+import Home from './pages/Home/Home.jsx'
 
-function App() {
+const App = () => {
   return (
-    <Layout>
-      <Routes>
-        <Route path="/" element={<Home />} />
-        <Route path="counter" element={<Counter />} />
-        <Route path="users" element={<Users />} />
-        <Route path="about" element={<About />} />
-      </Routes>
-    </Layout>
-  );
+    <div>
+      <Home/>
+    </div>
+  )
 }
 
-export default App;
+export default App
--- frontend/src/pages/Home/Home.jsx
+++ frontend/src/pages/Home/Home.jsx
@@ -1,25 +1,38 @@
-import logo from './logo.svg';
-import './Home.css';
+import React from 'react'
+import './Home.css'
+import Navbar from '../../components/NavBar/Navbar'
+import hero_banner from '../../assets/hero_banner.jpg'
+import hero_title from '../../assets/hero_title.png'
+import play_icon from '../../assets/play_icon.png'
+import info_icon from '../../assets/info_icon.png'
+import TitleCards from '../../components/TitleCards/TitleCards'
+import Footer from '../../components/Footer/Footer'
 
-function Home() {
+const Home = () => {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.jsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://react.dev"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className='home'>
+      <Navbar/>
+      <div>
+        <img src={hero_banner} alt="" className='banner-img'/>
+        <div className="hero-caption">
+          <img src={hero_title} alt="" className='caption-img'/>
+          <p>Melek </p>
+          <div className="hero-btns">
+            <button className='btn'><img src={play_icon} alt="" />Play</button>
+            <button className='btn dark-btn'><img src={info_icon} alt="" />More Info</button>
+          </div>
+          <TitleCards/>
+        </div>
+      </div>
+      <div className="more-cards">
+        <TitleCards title={"BlockbusterMovie"}/>
+        <TitleCards title={"Only on CSFlix"}/>
+        <TitleCards title={"Upcoming"}/>
+        <TitleCards title={"Top pics for you"}/>
+      </div>
+      <Footer/>
     </div>
-  );
+  )
 }
 
 export default Home;
